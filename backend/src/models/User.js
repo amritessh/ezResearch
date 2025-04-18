41c5346d3a3e741a8d8e05c0e9cee40ccd5a4187
Amritesh Anand <amriteshanand7@gmail.com>
2025-04-18 14:47:45 -0400

    user schema modeling done

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -34,3 +34,22 @@ const UserSchema = new mongoose.Schema({
     type: Date
   }
 });
+
+//password hashing middleware
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+UserSchema.methods.isValidPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
